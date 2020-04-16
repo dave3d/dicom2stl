804ddce1188d298a3bcb1a30557995770c1add9f
Dave Chen <dchen@mail.nih.gov>
2020-04-16 17:03:10 -0400

    completed test_loadZipDicom

--- tests/test_dicomutils.py
+++ tests/test_dicomutils.py
@@ -1,40 +1,43 @@
 #! /usr/bin/env python
 
 import unittest
-import os, shutil
-from utils import dicomutils
+import os, shutil, zipfile
 import vtk
 import SimpleITK as sitk
+
+from utils import dicomutils
 from tests import create_data
 from tests import write_series
 
 class TestDicomUtils(unittest.TestCase):
 
     TMPDIR = "testtmp"
+    SIZE = 32
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         print("\nBuildin\' it up!")
-        cyl = create_data.make_cylinder(32, sitk.sitkUInt16)
+        cyl = create_data.make_cylinder(TestDicomUtils.SIZE, sitk.sitkUInt16)
         try:
-            os.mkdir(self.TMPDIR)
+            os.mkdir(TestDicomUtils.TMPDIR)
         except:
             print("Oopsie")
-        write_series.write_series( cyl, self.TMPDIR )
+        write_series.write_series( cyl, TestDicomUtils.TMPDIR )
 
-
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         print("\nTearin\' it down!")
-        shutil.rmtree(self.TMPDIR)
+        shutil.rmtree(TestDicomUtils.TMPDIR)
 
-    def test_dicomutils(self):
-
+    def test_scanDirForDicom(self):
         print("\nTesting DicomUtils.scanDirForDicom")
-        matches, dirs = dicomutils.scanDirForDicom(self.TMPDIR)
+        matches, dirs = dicomutils.scanDirForDicom(TestDicomUtils.TMPDIR)
         print(matches, dirs)
-        self.assertEqual(len(matches), 32)
+        self.assertEqual(len(matches), TestDicomUtils.SIZE)
 
+    def test_getAllSeries(self):
         print("\nTesting DicomUtils.getAllSeries")
-        seriessets = dicomutils.getAllSeries([self.TMPDIR])
+        seriessets = dicomutils.getAllSeries([TestDicomUtils.TMPDIR])
         print(seriessets)
         self.assertEqual(len(seriessets),1)
         series_id = seriessets[0][0]
@@ -42,6 +45,35 @@ class TestDicomUtils(unittest.TestCase):
            print("    Series looks good")
         else:
            self.fail("    Bad series: "+series_id)
+
+    def test_getModality(self):
+        print("\nTesting DicomUtils.getModality")
+        img = sitk.Image(10,10,sitk.sitkUInt16)
+        m1 = dicomutils.getModality(img)
+        self.assertEqual(m1, "")
+        img.SetMetaData("0008|0060", "dude")
+        m2 = dicomutils.getModality(img)
+        self.assertEqual(m2, "dude")
+
+    def test_loadLargestSeries(self):
+        print("\nTesting DicomUtils.loadLargestSeries")
+        img, mod = dicomutils.loadLargestSeries(TestDicomUtils.TMPDIR)
+        self.assertEqual(img.GetSize(), (TestDicomUtils.SIZE,TestDicomUtils.SIZE,TestDicomUtils.SIZE))
+        self.assertEqual(mod, "CT")
+
+    def test_loadZipDicom(self):
+        print("\nTesting DicomUtils.loadZipDicom")
+        zf = zipfile.ZipFile('tests/testzip.zip', 'w')
+        for z in range(TestDicomUtils.SIZE):
+            zf.write(TestDicomUtils.TMPDIR+'/'+str(z)+'.dcm')
+        zf.close()
+        img, mod = dicomutils.loadZipDicom('tests/testzip.zip', 'tests/ziptmp')
+        print(img.GetSize())
+        print(mod)
+        os.unlink('tests/testzip.zip')
+        shutil.rmtree('tests/ziptmp')
+
+
 
 if __name__ == "__main__":
     unittest.main()
