385565d28a97cf6baa0b2c89960fa20494e3eea1
Dave Chen <dchen@mail.nih.gov>
2020-06-24 16:07:22 -0400

    new argument parser checkin

---
+++ parseargs.py
@@ -0,0 +1,109 @@
+#! /usr/bin/env python
+
+import argparse
+
+class disableFilter(argparse.Action):
+    def __call__(self, parser, args, values, option_string=None):
+       print("action, baby!", self.dest, values)
+       print(args, type(args))
+       x = getattr(args, self.dest)
+       print("getattr:", x, type(x))
+       #setattr(args, self.dest, values)
+       noval = 'no'+values
+       x.append(noval)
+
+class enableAnisotropic(argparse.Action):
+    def __init__(self, nargs=0, **kw):
+        super().__init__(nargs=nargs, **kw)
+    def __call__(self, parser, args, values, option_string=None):
+        x = getattr(args, 'filters')
+        x.append('anisotropic')
+
+class enableLargest(argparse.Action):
+    def __init__(self, nargs=0, **kw):
+        super().__init__(nargs=nargs, **kw)
+    def __call__(self, parser, args, values, option_string=None):
+        x = getattr(args, 'filters')
+        x.append('largest')
+
+def parseargs():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--verbose', '-v', action='store_true', default=False,
+                        dest='verbose', help='Enable verbose messages')
+
+    parser.add_argument('--debug', '-D', action='store_true', default=False,
+                        dest='debug',
+                        help="""Enable debugging messages
+                        """)
+
+    parser.add_argument('--output', '-o', action='store', dest='output',
+                        default='result.stl',
+                        help='Output file name (default=result.stl)')
+
+    parser.add_argument('--meta', '-m', action='store', dest='meta',
+                        help='Output metadata file')
+
+    parser.add_argument('--ct', action='store_true', default=False,
+                        dest='ctonly', help='Only allow CT Dicom as input')
+
+    parser.add_argument('--clean', '-c', action='store_true', default=False,
+                        dest='clean', help='Clean up temp files')
+
+    parser.add_argument('--temp', '-T', action='store', dest='temp',
+                        help='Temporary directory')
+
+    parser.add_argument('--search', '-s', action='store', dest='search',
+                        help='Dicom series search string')
+
+    # Options that apply to the volumetric portion of the pipeline
+    vol_group = parser.add_argument_group('Volume options')
+
+    vol_group.add_argument('--type', '-t', action='store', dest='type',
+                           choices=['skin', 'bone', 'soft_tissue', 'fat'],
+                           help='CT tissue type')
+
+    vol_group.add_argument('--anisotropic', '-a', action=enableAnisotropic,
+                           help='Apply anisotropic smoothing to the volume')
+
+    vol_group.add_argument('--isovalue', '-i', action='store', dest='isovalue',
+                           type=float, default=0.0, help='Iso-surface value')
+
+    vol_group.add_argument('--double', '-d', action='store',
+                           dest='double_threshold',
+                           help="""Double threshold with 4 semicolon separated floats
+                                """)
+
+    # Options that apply to the mesh processing portion of the pipeline
+    mesh_group = parser.add_argument_group('Mesh options')
+    mesh_group.add_argument('--largest', '-l', action=enableLargest,
+                            help='Only keep the largest connected sub-mesh')
+
+    mesh_group.add_argument('--rotaxis', action='store', dest='rotaxis',
+                            default='Y', choices=['X', 'Y', 'Z'],
+                            help='Rotation axis (default=Y)')
+
+    mesh_group.add_argument('--rotangle', action='store', dest='rotaxis',
+                            type=float, default=0.0,
+                            help='Rotation angle in degrees (default=180)')
+
+    mesh_group.add_argument('--smooth', action='store', dest='smooth',
+                            type=int, default=25,
+                            help='Mesh smoothing iterations (default=25)')
+
+    mesh_group.add_argument('--reduce', action='store', dest='reduce',
+                            type=float, default=.9,
+                            help='Mesh reduction factor (default=.9)')
+
+    # Filtering options
+    filter_group = parser.add_argument_group('Filtering options')
+    filter_group.add_argument('--enable', action='append', dest='filters',
+                              choices=['anisotropic', 'shrink',
+                                       'median', 'largest', 'rotation'],
+                              help='Enable filtering options')
+    filter_group.add_argument('--disable', action=disableFilter, dest='filters',
+                              choices=['anisotropic', 'shrink', 'median',
+                                       'largest', 'rotation'],
+                              help='Disable filtering options')
+    args = parser.parse_args()
+    return args
