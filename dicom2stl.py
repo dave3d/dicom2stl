3f790344645a3dcc14baebca5982704ea932a4e4
Dave Chen <dchen@mail.nih.gov>
2020-06-25 16:59:04 -0400

    re-factor to use new cli parser

--- dicom2stl.py
+++ dicom2stl.py
@@ -17,7 +17,6 @@ ordered the same alphabetically as they are physically.
 from __future__ import print_function
 
 import gc
-import getopt
 import math
 import os
 import sys
@@ -28,182 +27,57 @@ import zipfile
 import vtk
 import SimpleITK as sitk
 
+import parseargs
+
 from utils import sitk2vtk
 from utils import dicomutils
 from utils import vtkutils
 
 # Global variables
 #
-verbose = 0
-debug = 0
 
 zipFlag = False
-dicomString = ""
-cleanUp = False
-tempDir = ""
 dirFlag = False
 
-isovalue = 0
-CTonly = False
-doubleThreshold = False
-thresholds = []
-tissueType = ""
 shrinkFlag = True
-
-smoothIterations = 25
-quad = .90
-outname = "result.stl"
+thesholds = []
+shrinkFlag = True
 connectivityFilter = False
 anisotropicSmoothing = False
 medianFilter = False
-metadataFile = ""
-modality = ""
-
 rotFlag = False
-rotAxis = 1
-rotAngle = 180
 
-options = []
+args = parseargs.parseargs()
 
+isovalue = args.isovalue
 
-def usage():
-    print("")
-    print("dicom2stl.py: [options] dicom.zip")
-    print("   or")
-    print("dicom2stl.py: [options] volume_image")
-    print("   or")
-    print("dicom2stl.py: [options] slice1 ... sliceN")
-    print("   or")
-    print("dicom2stl.py: [options] dicom_directory")
-    print("")
-    print("  -h, --help          This help message")
-    print("  -v, --verbose       Verbose output")
-    print("  -D, --debug         Debug mode")
-    print("")
-    print("  -o string           Output file name (default=result.stl)")
-    print("  -m string           Metadata file name (default=\"\")")
-    print("  --ct                Only allow CT images")
-    print("  -c, --clean         Clean up temp files")
-    print("  -T string, --temp string      Directory to place temporary files")
-    print("  -s string, --search string    Dicom series search string")
-    print("")
-    print("  Volume processing options")
-    print(
-        "  -t string, --type string      CT Tissue type",
-        "[skin, bone, soft_tissue, fat]")
-    print("  -a, --anisotropic            ",
-          "Apply anisotropic smoothing to the volume")
-    print("  -i num, --isovalue num        Iso-surface value")
-    print("  -d string, --double string   ",
-          "Double threshold with 4 values in a string seperated by semicolons")
-    print("")
-    print("  Mesh options")
-    print("  -l, --largest       Keep only the largest connected mesh")
-    print("  --rotaxis int       Rotation axis (default=1, Y-axis)")
-    print("  --rotangle float    Rotation angle (default=180 degrees)")
-    print("  --smooth int        Smoothing iterations (default=25)")
-    print("  --reduce float      Polygon reduction factor (default=.9)")
-    print("")
-    print("  Enable/Disable various filtering options")
-    print(
-        "  --disable string   ",
-        "Disable an option [anisotropic, shrink, median, largest, rotation]")
-    print(
-        "  --enable  string   ",
-        "Enable an option [anisotropic, shrink, median, largest, rotation]")
+# Handle enable/disable filters
 
-
-# Parse the command line arguments
-#
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "vDhacli:s:t:d:o:m:T:",
-                               ["verbose", "help", "debug", "anisotropic",
-                                "clean", "ct", "isovalue=", "search=", "type=",
-                                "double=", "disable=", "enable=", "largest",
-                                "metadata", "rotaxis=", "rotangle=", "smooth=",
-                                "reduce=", "temp="])
-except getopt.GetoptError as err:
-    print(str(err))
-    usage()
-    sys.exit(2)
-
-for o, a in opts:
-    if o in ("-v", "--verbose"):
-        verbose = verbose + 1
-    elif o in ("-D", "--debug"):
-        print("Debug")
-        debug = debug + 1
-        cleanFlag = False
-    elif o in ("-h", "--help"):
-        usage()
-        sys.exit()
-    elif o in ("-c", "--clean"):
-        cleanUp = True
-    elif o in ("-T", "--temp"):
-        tempDir = a
-    elif o in ("-a", "--anisotropic"):
-        anisotropicSmoothing = True
-    elif o in ("-i", "--isovalue"):
-        isovalue = float(a)
-    elif o in ("--ct"):
-        CTonly = True
-    elif o in ("-l", "--largest"):
-        connectivityFilter = True
-    elif o in ("-s", "--search"):
-        dicomString = a
-    elif o in ("-t", "--type"):
-        tissueType = a
-        doubleThreshold = True
-    elif o in ("-o", "--output"):
-        outname = a
-    elif o in ("-m", "--metadata"):
-        metadataFile = a
-    elif o in ("-d", "--double"):
-        vals = a.split(';')
-        for v in vals:
-            thresholds.append(float(v))
-        thresholds.sort()
-        doubleThreshold = True
-    elif o in ("--rotaxis"):
-        rotAxis = int(a)
-    elif o in ("--rotangle"):
-        rotAngle = float(a)
-    elif o in ("--smooth"):
-        smoothIterations = int(a)
-    elif o in ("--reduce"):
-        quad = float(a)
-    elif o in ("--disable"):
-        options.append("no" + a)
-    elif o in ("--enable"):
-        options.append(a)
-    else:
-        assert False, "unhandled options"
-
-# Handle enable/disable options
-
-for x in options:
-    val = True
-    y = x
-    if x[:2] == "no":
-        val = False
-        y = x[2:]
-    if y.startswith("shrink"):
-        shrinkFlag = val
-    if y.startswith("aniso"):
-        anisotropicSmoothing = val
-    if y.startswith("median"):
-        medianFilter = val
-    if y.startswith("large"):
-        connectivityFilter = val
-    if y.startswith("rotat"):
-        rotFlag = val
+if args.filters:
+    for x in args.filters:
+        val = True
+        y = x
+        if x[:2] == "no":
+            val = False
+            y = x[2:]
+        if y.startswith("shrink"):
+            shrinkFlag = val
+        if y.startswith("aniso"):
+            anisotropicSmoothing = val
+        if y.startswith("median"):
+            medianFilter = val
+        if y.startswith("large"):
+            connectivityFilter = val
+        if y.startswith("rotat"):
+            rotFlag = val
 
 print("")
+tempDir = args.temp
 if tempDir == "":
     tempDir = tempfile.mkdtemp()
 print("Temp dir: ", tempDir)
 
+tissueType = args.tissue
 if tissueType:
     # Convert tissue type name to threshold values
     print("Tissue type: ", tissueType)
@@ -218,16 +92,20 @@ if tissueType:
         thresholds = [-122., -112., -96., -70.]
         medianFilter = True
 
-if doubleThreshold:
+if args.double_threshold:
+    words = args.double_threshold.split(';')
+    thresholds = []
+    for x in words:
+        thresholds.append(float(x))
     # check that there are 4 threshold values.
     print("Thresholds: ", thresholds)
     if len(thresholds) != 4:
-        print("Error: Threshold is not of size 4.", thresholds)
+        print("Error: Thresholds is not of len 4.", thresholds)
         sys.exit(3)
 else:
     print("Isovalue = ", isovalue)
 
-fname = args
+fname = args.filenames
 
 if len(fname) == 0:
     print("Error: no input given.")
@@ -248,7 +126,7 @@ else:
         print("File names: ", fname, "\n")
 
 
-if debug:
+if args.debug:
     print("SimpleITK version: ", sitk.Version.VersionString())
     print("SimpleITK: ", sitk, "\n")
 
@@ -261,29 +139,29 @@ metasrc = img
 #
 if zipFlag:
     # Case for a zip file of images
-    if verbose:
+    if args.verbose:
         print("zip")
     img, modality = dicomutils.loadZipDicom(fname[0], tempDir)
 
 
 else:
     if dirFlag:
-        if verbose:
+        if args.verbose:
             print("directory")
         img, modality = dicomutils.loadLargestSeries(fname[0])
 
     else:
         # Case for a single volume image
         if len(fname) == 1:
-            if verbose:
+            if args.verbose:
                 print("Reading volume: ", fname[0])
             img = sitk.ReadImage(fname[0])
             modality = dicomutils.getModality(img)
 
         else:
             # Case for a series of image files
-            if verbose:
-                if verbose > 1:
+            if args.verbose:
+                if args.verbose > 1:
                     print("Reading images: ", fname)
                 else:
                     print("Reading images: ",
@@ -294,8 +172,8 @@ else:
             firstslice = sitk.ReadImage(fname[0])
             modality = dicomutils.getModality(firstslice)
 
-if CTonly and ((sitk.Version.MinorVersion() > 8)
-               or (sitk.Version.MajorVersion() > 0)):
+if args.ctonly and ((sitk.Version.MinorVersion() > 8)
+                    or (sitk.Version.MajorVersion() > 0)):
     # Check the metadata for CT image type.  Note that this only works with
     # SimpleITK version 0.8.0 or later.  For earlier versions there is no
     # GetMetaDataKeys method
@@ -319,8 +197,8 @@ def elapsedTime(start_time):
 
 # Write out the metadata text file
 #
-if len(metadataFile):
-    FP = open(metadataFile, "w")
+if args.meta:
+    FP = open(args.meta, "w")
     size = img.GetSize()
     spacing = img.GetSpacing()
     FP.write('xdimension ' + str(size[0]) + '\n')
@@ -368,7 +246,7 @@ if anisotropicSmoothing:
 
 # Apply the double threshold filter to the volume
 #
-if doubleThreshold:
+if args.double_threshold:
     print("Double Threshold")
     t = time.process_time()
     img = sitk.DoubleThreshold(
@@ -394,13 +272,13 @@ pad = [5, 5, 5]
 img = sitk.ConstantPad(img, pad, pad)
 gc.collect()
 
-if verbose:
+if args.verbose:
     print("\nImage for isocontouring")
     print(img.GetSize())
     print(img.GetPixelIDTypeAsString())
     print(img.GetSpacing())
     print(img.GetOrigin())
-    if verbose > 1:
+    if args.verbose > 1:
         print(img)
     print("")
 
@@ -413,41 +291,43 @@ vtkimg = sitk2vtk.sitk2vtk(img)
 img = None
 gc.collect()
 
-if debug:
+if args.debug:
     print("\nVTK version: ", vtk.vtkVersion.GetVTKVersion())
     print("VTK: ", vtk, "\n")
 
-if debug:
+if args.debug:
     print("Extracting surface")
 mesh = vtkutils.extractSurface(vtkimg, isovalue)
 vtkimg = None
 gc.collect()
-if debug:
+if args.debug:
     print("Cleaning mesh")
 mesh2 = vtkutils.cleanMesh(mesh, connectivityFilter)
 mesh = None
 gc.collect()
-if debug:
-    print("Smoothing mesh", smoothIterations, "iterations")
-mesh3 = vtkutils.smoothMesh(mesh2, smoothIterations)
+if args.debug:
+    print("Smoothing mesh", args.smooth, "iterations")
+mesh3 = vtkutils.smoothMesh(mesh2, args.smooth)
 mesh2 = None
 gc.collect()
-if debug:
+if args.debug:
     print("Simplifying mesh")
-mesh4 = vtkutils.reduceMesh(mesh3, quad)
+mesh4 = vtkutils.reduceMesh(mesh3, args.reduce)
 mesh3 = None
 gc.collect()
 
-if rotFlag:
-    mesh5 = vtkutils.rotateMesh(mesh4, rotAxis, rotAngle)
+axis_map = {'X': 0, 'Y': 1, 'Z': 2}
+rotAxis = axis_map[args.rotaxis]
+if args.rotangle != 0.0:
+    mesh5 = vtkutils.rotateMesh(mesh4, rotAxis, args.rotangle)
 else:
     mesh5 = mesh4
-vtkutils.writeMesh(mesh5, outname)
+vtkutils.writeMesh(mesh5, args.output)
 mesh4 = None
 gc.collect()
 
 # remove the temp directory
-if cleanUp:
+if args.clean:
     shutil.rmtree(tempDir)
 
 print("")
