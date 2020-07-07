4b3aeeddc4bae484d2682ca54e8725e68f0f8c2e
Dave Chen <dchen@mail.nih.gov>
2020-07-07 14:38:43 -0400

    do a real test

--- tests/test_dicom2stl.py
+++ tests/test_dicom2stl.py
@@ -1,17 +1,37 @@
 #! /usr/bin/env python
 
 import unittest
+import subprocess
+import os
 
 import SimpleITK as sitk
 
+from tests import create_data
 
-import create_data
 
 class TestDicom2STL(unittest.TestCase):
 
-    def test_dicom2stl(self):
+    @classmethod
+    def setUpClass(self):
+        print("Setting up dicom2stl tests")
         img = create_data.make_tetra()
-        sitk.WriteImage(img, "tetra.nii.gz")
+        sitk.WriteImage(img, "tetra-test.nii.gz")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Tearing down dicom2stl tests")
+        os.remove("tetra-test.nii.gz")
+        os.remove("testout.stl")
+
+    def test_dicom2stl(self):
+        print("Dicom2stl test")
+        runresult = subprocess.run(['dicom2stl.py', '-i', '100', '-o',
+                                    'testout.stl', 'tetra-test.nii.gz'])
+        print(runresult.returncode)
+        if runresult.returncode:
+            self.fail("dicom2stl process returned bad code")
+        if not os.path.exists("testout.stl"):
+            self.fail("dicom2stl: no output file")
 
 
 if __name__ == "__main__":
