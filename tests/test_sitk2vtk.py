41dae55aaa3e4075d71e19e9e0266fccdef459c6
Dave Chen <dchen@mail.nih.gov>
2020-07-08 11:12:13 -0400

    Removed old Windows hack

--- tests/test_sitk2vtk.py
+++ tests/test_sitk2vtk.py
@@ -14,19 +14,12 @@ class TestSITK2VTK(unittest.TestCase):
         dims = [102, 102, 102]
         img = sitk.GaussianSource(sitk.sitkUInt8, dims)
 
-        if platform.system() == "Windows":
-            invol = vtk.vtkImageData()
-            invol.SetDimensions(10, 10, 10)
-            vol = sitk2vtk.sitk2vtk(img, invol, True)
-            print("Accessing VTK image")
-            print(invol.GetScalarComponentAsFloat(5, 5, 5, 0))
-        else:
-            vol = sitk2vtk.sitk2vtk(img, True)
-            self.assertTupleEqual(vol.GetDimensions(), tuple(dims))
-            print("Accessing VTK image")
-            val = vol.GetScalarComponentAsFloat(5, 5, 5, 0)
-            print(val)
-            self.assertAlmostEqual(val, 3.0)
+        vol = sitk2vtk.sitk2vtk(img, True)
+        self.assertTupleEqual(vol.GetDimensions(), tuple(dims))
+        print("Accessing VTK image")
+        val = vol.GetScalarComponentAsFloat(5, 5, 5, 0)
+        print(val)
+        self.assertAlmostEqual(val, 3.0)
 
 
 if __name__ == "__main__":
