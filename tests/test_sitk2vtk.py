9cecc6ef0d9117de970c7a48366e4c7463699c42
Dave Chen <dchen@mail.nih.gov>
2021-04-19 11:08:09 -0400

    added direction matrix for sitk2vtk

--- tests/test_sitk2vtk.py
+++ tests/test_sitk2vtk.py
@@ -13,13 +13,22 @@ class TestSITK2VTK(unittest.TestCase):
         print("Testing sitk2vtk")
         dims = [102, 102, 102]
         img = sitk.GaussianSource(sitk.sitkUInt8, dims)
+        direction = [0.0, 1.0, 0.0,  1.0, 0.0, 0.0,  0.0, 0.0, -1.0]
+        img.SetDirection(direction)
 
         vol = sitk2vtk.sitk2vtk(img, True)
         self.assertTupleEqual(vol.GetDimensions(), tuple(dims))
-        print("Accessing VTK image")
+        print("\nAccessing VTK image")
         val = vol.GetScalarComponentAsFloat(5, 5, 5, 0)
         print(val)
         self.assertAlmostEqual(val, 3.0)
+
+        if vtk.vtkVersion.GetVTKMajorVersion()>=9:
+            print("\nDirection matrix")
+            print(vol.GetDirectionMatrix())
+        else:
+            print("VTK version < 9.  No direction matrix")
+
 
 
 if __name__ == "__main__":
