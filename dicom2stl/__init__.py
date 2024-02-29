971cf679a14f443a5606fd00df84e36adb1108ee
Dave Chen <dchen@mail.nih.gov>
2024-02-28 20:08:52 -0500

    wrong dicom2stl module

--- dicom2stl/__init__.py
+++ dicom2stl/__init__.py
@@ -1,5 +1,6 @@
+import dicom2stl
 import dicom2stl.utils.parseargs
 def main():
     """Entry point for the application script"""
     args = dicom2stl.utils.parseargs.parseargs()
-    dicom2stl_func.Dicom2STL(args)
+    dicom2stl.Dicom2STL(args)
